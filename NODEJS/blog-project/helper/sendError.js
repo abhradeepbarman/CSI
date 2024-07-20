c1c6a43977af2e53cde29eb37703840daf9fa652
abhradeepbarman <abhradeep709@gmail.com>
2024-07-20 15:41:40 +0530

    blog project added

--- NODEJS/blog-project/helper/sendError.js
+++
@@ -1,8 +0,0 @@
-const sendErrorMessage = (error, req, res) => {
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
-};
-
-module.exports = sendErrorMessage;
--- NODEJS/blog-project/helper/sendResponse.js
+++
@@ -1,9 +0,0 @@
-const sendResponse = (statusCode, status, data, req, res) => {
-  if (data) {
-    res.status(statusCode).json({ status: status, data: [data] });
-  } else {
-    res.status(statusCode).json({ status: status });
-  }
-};
-
-module.exports = sendResponse;
